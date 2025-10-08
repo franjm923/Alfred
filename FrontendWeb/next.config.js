58fa50b299bdfa14f97818b9f5092b6b2277983d
Francisco Juarez <franjuarez2013@gmail.com>
2025-10-08 01:20:50 -0300

    Index

--- FrontendWeb/next.config.js
+++ FrontendWeb/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() { return []; },
+  async rewrites() {
+    return [
+      { source: '/', destination: '/index.html' },
+    ];
+  },
   async redirects() { return []; },
   async headers()  { return []; },
 };
